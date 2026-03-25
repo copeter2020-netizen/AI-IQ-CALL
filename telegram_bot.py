9008d9006816d9cf766832cb0f36b275556cf501
copeter2020-netizen <copeter2020@gmail.com>
2026-03-25 18:47:52 -0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,26 +1,27 @@
 import os
 import requests
 
-# ==========================
-# CONFIG TELEGRAM
-# ==========================
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
-CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
 def send_message(text):
 
     if not TOKEN or not CHAT_ID:
+        print("❌ TELEGRAM NO CONFIGURADO")
         return
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    data = {
-        "chat_id": CHAT_ID,
-        "text": text
-    }
-
     try:
-        requests.post(url, data=data, timeout=5)
-    except:
-        pass
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+        requests.post(
+            url,
+            data={
+                "chat_id": CHAT_ID,
+                "text": text
+            },
+            timeout=5
+        )
+
+    except Exception as e:
+        print("❌ Error Telegram:", e)
