6b41d229ef7db84c3b4f29882ae569fc9ea9afa5
copeter2020-netizen <copeter2020@gmail.com>
2026-04-19 09:35:35 -0500

    Update estrategia.py

--- estrategia.py
+++ estrategia.py
@@ -16,25 +16,16 @@ def check_buy_signal(df):
     if len(df) < 6:
         return False
 
-    prev = df.iloc[-5]   # vela anterior al impulso
-    c3 = df.iloc[-4]     # vela de impulso (roja)
-    c4 = df.iloc[-3]     # primera confirmación
-    c5 = df.iloc[-2]     # segunda confirmación
+    prev = df.iloc[-5]
+    c3 = df.iloc[-4]
+    c4 = df.iloc[-3]
+    c5 = df.iloc[-2]
 
     return (
-        # tendencia
         c3['close'] < c3['ema_100'] and
-
-        # toque banda inferior
         c3['close'] <= c3['lower_band'] and
-
-        # vela roja
         c3['close'] < c3['open'] and
-
-        # 🔥 cruce en la MISMA vela c3
         (c3['upper_band'] > c3['ema_100'] and prev['upper_band'] <= prev['ema_100']) and
-
-        # confirmaciones
         c4['close'] > c4['open'] and
         c5['close'] > c5['open']
     )
@@ -50,19 +41,10 @@ def check_sell_signal(df):
     c5 = df.iloc[-2]
 
     return (
-        # tendencia
         c3['close'] > c3['ema_100'] and
-
-        # toque banda superior
         c3['close'] >= c3['upper_band'] and
-
-        # vela verde
         c3['close'] > c3['open'] and
-
-        # 🔥 cruce en la MISMA vela c3
         (c3['lower_band'] < c3['ema_100'] and prev['lower_band'] >= prev['ema_100']) and
-
-        # confirmaciones
         c4['close'] < c4['open'] and
         c5['close'] < c5['open']
     )
